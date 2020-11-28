b33c02b584a4f74991669a2e32a0f8a947df61ab
Mike Babb <mike7400@gmail.com>
2020-11-27 19:16:24 -0500

    Added bisect right + other bisect changes.

--- data_structures/utils.py
+++ data_structures/utils.py
@@ -1,12 +1,19 @@
 from typing import *
 
+
 T = TypeVar("T")
 Comparator = Callable[[T, T], int]
 
 default_comparator = lambda x, y: -1 if x < y else 1 if x > y else 0
 
 
-def bisect_left(arr: List[T], x: T, comparator: Comparator = default_comparator) -> int:
+def _bisect(
+    arr: List[T],
+    x: T,
+    comparator: Comparator = default_comparator,
+    left: bool = True,
+    negate_found: bool = True,
+) -> int:
     def recurse(low: int, high: int) -> int:
         midpoint = (high + low) // 2
 
@@ -20,8 +27,37 @@ def bisect_left(arr: List[T], x: T, comparator: Comparator = default_comparator)
             elif comp > 0:
                 return recurse(midpoint + 1, high)
             else:
-                return midpoint
+                return midpoint if left else midpoint + 1
         else:
-            return -1 * (low + 1)
+            return -1 * (low + 1) if negate_found else low
 
     return recurse(0, len(arr) - 1)
+
+
+def bisect_left(
+    arr: List[T],
+    x: T,
+    comparator: Comparator = default_comparator,
+    negate_found: bool = True,
+) -> int:
+    return _bisect(arr, x, comparator, True, negate_found)
+
+
+def bisect_right(
+    arr: List[T],
+    x: T,
+    comparator: Comparator = default_comparator,
+    negate_found: bool = True,
+) -> int:
+    return _bisect(arr, x, comparator, False, negate_found)
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 4]
+
+    x = 3
+
+    ix = bisect_right(arr, x)
+    ix = -1 * (ix + 1)
+
+    print("h")
