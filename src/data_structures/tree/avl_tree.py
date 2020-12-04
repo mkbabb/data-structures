d7e7ff0e4def3b75969729e33ed053ed0183a5f0
Sanjana <sanjucheerla@gmail.com>
2020-12-03 23:18:09 -0500

    Fixed AVL tree to use BinaryTree

--- src/data_structures/tree/avl_tree.py
+++ src/data_structures/tree/avl_tree.py
@@ -9,27 +9,12 @@ T = TypeVar("T")
 
 #might need avl tree node that
 #has an additional property of height
-class AVLTree(Tree[T]):
-    def __init__(
-            self,
-            value: Optional[T] = None,
-            left: Optional["BinaryNode[T]"] = None,
-            right: Optional["BinaryNode[T]"] = None,
-            parent: Optional["BinaryNode[T]"] = None,
-        ) -> None:
-            super().__init__(tree_order=1, children=[left, right], parent=parent)
-            
-            if value is not None:
-                self.values.append(value)
+class AVLTree(BinaryTree[T]):
+    def __init__(self, comparator: Comparator = default_comparator):
+        super().__init__(1, comparator)
 
     def _on_insert(self, input_value: T, value_ix: int, node: BinaryNode[T]) -> None:
-        if node.is_root() and node.is_empty():
-            node.values.append(input_value)
-        else:
-            child = BinaryNode(input_value, parent=node)
-            node.children[value_ix] = child
-            self.compute_height()
-
+        super()._on_insert(1, input_value, value_ix, node)
 
     def compute_height(self):
         return
