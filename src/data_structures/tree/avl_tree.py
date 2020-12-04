6c10a10766ce89ae2a2cc788abd6a032b7ceddc8
Sanjana <sanjucheerla@gmail.com>
2020-12-04 16:21:28 -0500

    changed on_insert

--- src/data_structures/tree/avl_tree.py
+++ src/data_structures/tree/avl_tree.py
@@ -10,13 +10,13 @@ T = TypeVar("T")
 class AVLTreeNode(BinaryNode[T]):
     def __init__(
         self,
-        height: int,
         value: Optional[T] = None,
         left: Optional["BinaryNode[T]"] = None,
         right: Optional["BinaryNode[T]"] = None,
         parent: Optional["BinaryNode[T]"] = None,
     ) -> None:
-        self.height = height
+        self.height = 1
+        self.bf = 0
         super().__init__(tree_order=1, children=[left, right], parent=parent)
         
         if value is not None:
@@ -30,6 +30,14 @@ class AVLTreeNode(BinaryNode[T]):
     def height(self, other = int):
         self.height = other
 
+    @property
+    def balance_factor(self) -> int:
+        return self.balance_factor
+
+    @balance_factor.setter
+    def balance_factor(self, other = int):
+        self.bf = other
+
 
 class AVLTree(BinaryTree[T]):
     def __init__(self, comparator: Comparator = default_comparator):
@@ -37,12 +45,29 @@ class AVLTree(BinaryTree[T]):
 
     def _on_insert(self, input_value: T, value_ix: int, node: BinaryNode[T]) -> None:
         super()._on_insert(1, input_value, value_ix, node)
+        node.height = max(node.left.height() - node.right.height()) + 1
+        node.bf = node.left.height() - node.right.height()
+        self.rebalance()
 
-    def compute_height(self):
-        return
+    #return the index where you need to rebalance
+    def compute_height(self, AVLTreeNode = AVLTreeNode[T]) -> int:
+        if self is None:
+            return 0
+        else:
+            return AVLTreeNode.height()
 
-    def rebalance(self):
-        return
+    def update_heights(self) -> None:
+        if self is None:
+            self.height = -1
+        else:
+            return
 
-    def _on_delete(self, child_ix: int, node: TreeNode[T], value: T) -> None:
-        return
+    def rebalance(self) -> None:
+        if self.compute_heights() is not -1:
+            return
+        #balance if needed
+        self.update_heights()
+
+    def _delete(self, input_value: T) -> None:
+        super()._delete(self, input_value)
+        self.rebalance()
